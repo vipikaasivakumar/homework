9f70ef785cabb2b64f85b40e77b432ce98438e8e
vipikaasivakumar <vipikaasivakumar@gmail.com>
2026-02-14 17:27:12 -0500

    lesson 4 hw

--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,16 @@
 # Lesson 4
+from math import ceil
+
+section1 = input("Enter section 1: ") 
+section2 = input("Enter section 2: ")
+section3 = input("Enter section 3: ")
+
+cans = len(section1) + len(section2) + len(section3)
+boxes = ceil(cans/12) # rounding up to have enough paint cans
+
+totalCost = 14.95*boxes
+leftover = 12*boxes - cans
+
+print(f"You need {cans} cans.")
+print(f"You would have {leftover} cans leftover.")
+print(f"It would cost you ${totalCost}.")
