cf8180ac9a50fdc2cc0d0bbe66f038c2204cb112
vipikaasivakumar <vipikaasivakumar@gmail.com>
2026-02-14 17:13:12 -0500

    lesson 3 hw

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,6 @@
 # Lesson 3
+#logic is to use square root on tiles to find the optimized length
+from math import sqrt, floor #import statements
+tiles = int(input("Enter a positive integer: "))
+length = floor(sqrt(tiles))
+print(f"The largest square has a side length of {length}")
