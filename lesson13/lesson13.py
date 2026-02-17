d38ec37f540b2d2c8dd2309fd08c312a7223bb7c
vipikaasivakumar <vipikaasivakumar@gmail.com>
2026-02-16 22:03:27 -0500

    lesson 13 hw

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,18 @@
 # Lesson 13
+month = int(input("Enter month in numeric value: "))
+date = int(input("Enter the date: "))
+
+if month == 2 and date == 18:
+    print("Today is a special day!")
+else:
+    if month < 2:
+        print ("Before special day :(. ")
+    elif month > 2:
+        print ("After special day :(. ")
+    else:
+        if date < 18:
+            print ("Before special day :(. ")
+        else:
+            print ("After special day :(. ")
+
+         
