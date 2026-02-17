d9a19a58210d00fc0a5986d62d30307481beb48d
vipikaasivakumar <vipikaasivakumar@gmail.com>
2026-02-16 22:19:03 -0500

    lesson 15 hw

--- lesson15/lesson15.py
+++ lesson15/lesson15.py
@@ -1,1 +1,28 @@
 # Lesson 15
+
+#Example of While Loop
+secret = True
+passcode = "hehe"
+
+while secret:
+    userInput = input ("Guess passcode: ")
+    if userInput == passcode:
+        print ("Correct!")
+        secret = False
+    else:
+        print ("Incorrect")
+
+print('\n')
+
+#Example of For Loop
+userInput = input("Search for a colour: ")
+colours = ['Red', 'Yellow', 'Green', 'Purple']
+found = False
+
+for colourName in colours:
+    if colourName == userInput:
+        print (f"{userInput} is found!")
+        found = True
+
+if not found:
+    print(f"{userInput} is not found.")
