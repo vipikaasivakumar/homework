ff2e443dbc7770ce2042909ff5439846b91d9505
vipikaasivakumar <vipikaasivakumar@gmail.com>
2026-02-17 11:16:56 -0500

    lesson 12 hw

--- lesson12/lesson12.py
+++ lesson12/lesson12.py
@@ -1,1 +1,14 @@
 # Lesson 12
+angle1 = int(input("Enter angle 1: "))
+angle2 = int(input("Enter angle 2: "))
+angle3 = int(input("Enter angle 3: "))
+
+if angle1+angle2+angle3 != 180:
+    print("Inputted angles do not create a triangle")
+else:
+    if angle1 == angle2 == angle3:
+        print("Equilateral triangle")
+    elif angle1 != angle2 and angle1 != angle3 and angle2 != angle3:
+        print("Scalene triangle")
+    else:
+        print("Isoceles triangle")
