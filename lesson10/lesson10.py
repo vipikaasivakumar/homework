f04b41b1e40f4599e30efefd1b80c47ce8f0b146
vipikaasivakumar <vipikaasivakumar@gmail.com>
2026-03-03 18:56:07 -0500

    lesson 10 hw

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,18 @@
 # Lesson 10
+phone = ''
+digits = True
+while digits:
+    phone  = input("Enter the last 4 digits of your phone number: ")
+    if len(phone) == 4:
+        digits = False
+
+if phone[0] == '8' or phone[0] == '9':
+    if phone[1] == phone[2]:
+        if phone[3] == '8' or phone[3] == '9': #if you are printing a statement, then else is necessary
+            print("This phone number is a telemarketer.")
+        else: 
+            print("This phone number is not a telemarketer.")
+    else:
+        print("This phone number is not a telemarketer.")
+else: 
+    print("This phone number is not a telemarketer.")
