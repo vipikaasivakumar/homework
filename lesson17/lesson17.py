781e35a948ff8a1396d35c17326ca1c37a1d3331
vipikaasivakumar <vipikaasivakumar@gmail.com>
2026-03-03 18:54:40 -0500

    lesson 17 hw

--- lesson17/lesson17.py
+++ lesson17/lesson17.py
@@ -1,1 +1,7 @@
 # Lesson 17
+#Factorial means the ! operator in math. Ex: 5! = 5x4x3x2x1
+num  = int(input("Enter a number: "))
+answer = 1
+for i in range(1, num+1): #this goes from 1 to num+1 (bc range doesn't count last number)
+    answer*=i
+print(f"Factorial of {num} is {answer}")
