e07ef37fae5fb259f87267e6c109e328159ca535
vipikaasivakumar <vipikaasivakumar@gmail.com>
2026-03-03 19:15:37 -0500

    lesson 19 hw

--- lesson19/lesson19.py
+++ lesson19/lesson19.py
@@ -1,1 +1,12 @@
-# Lesson 19
+# Lesson 19 for the hundredth time T-T
+num = int(input("Enter a number: "))
+isPrime = True
+
+for i in range(2, num): #start from 2 bc 1 is neither
+    if num%i == 0:
+        isPrime = False  #if divider (i) is factor it is not prime as i is not the number (num is exclusive in range)
+        break
+if isPrime:
+    print(f"{num} is prime.")
+else:
+    print(f"{num} is composite.")
