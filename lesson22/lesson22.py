80fe4f14974a30ddebb885267248b6ad295d8138
vipikaasivakumar <vipikaasivakumar@gmail.com>
2026-03-03 20:06:10 -0500

    lesson 22 hw

--- lesson22/lesson22.py
+++ lesson22/lesson22.py
@@ -1,1 +1,10 @@
 # Lesson 22
+fib0 = 0
+fib1 = 1
+
+num = int(input("Enter the term number: "))
+for i in range(2, num+1):
+    fib_num = fib1+fib0
+    fib0 = fib1
+    fib1 = fib_num
+print(f"Fibonacci's {num}th term is {fib_num}.")
