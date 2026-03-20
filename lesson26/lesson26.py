a76cc70b4326b7f1b9c139dfd4e0a084e2f52efc
vipikaasivakumar <vipikaasivakumar@gmail.com>
2026-03-20 17:14:56 -0400

    lesson 26 hw

--- lesson26/lesson26.py
+++ lesson26/lesson26.py
@@ -1,1 +1,28 @@
 # Lesson 26
+
+#Creation of function for example problem: Find number of factors for each number from 1 to N
+def isDivisible (num1, num2):
+    #checks if num1 is divisible by num2 and returns boolean
+    return num1%num2 == 0
+    
+def factorCount (num):
+    #Use of triple quotations to explain in longer formats
+    '''function determines factors of argument
+    Arg: num - an int neded to find numbers of its factors
+    Returns: an int which the total amount of factors
+    '''
+    counter = 0
+    for divider in range(1, num+1):
+        # if num % divider == 0:
+        if isDivisible(num, divider):
+            counter+=1
+    return counter
+
+
+upLimit = int(input("Enter N: "))
+
+for num in range(1, upLimit+1):
+    factorSize = factorCount(num) #use of function
+    print(f"{num} has {factorSize} factors.")
+
+
