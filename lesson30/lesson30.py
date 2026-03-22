9e0857108e50ac0a49b1a05b28ee5317d57509e0
vipikaasivakumar <vipikaasivakumar@gmail.com>
2026-03-22 16:00:39 -0400

    lesson 30 hw

--- lesson30/lesson30.py
+++ lesson30/lesson30.py
@@ -1,1 +1,18 @@
 # Lesson 30
+num = int(input("Enter an integer greater than zero :"))
+def pattern (num):
+    line = " "
+    for i in range(num):
+        if i%2 == 0:
+            line += "1"
+        else:
+            line += "0"
+    
+    return line
+
+def output(num):
+    for i in range(1, num+1):
+        print(pattern(i))
+
+ans = output(num)
+print(ans)
