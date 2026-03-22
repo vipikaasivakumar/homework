c15d586c17d4fa2c7fcea5ddba008704c39d327a
vipikaasivakumar <vipikaasivakumar@gmail.com>
2026-03-22 15:52:26 -0400

    lesson 29 hw

--- lesson29/lesson29.py
+++ lesson29/lesson29.py
@@ -1,1 +1,20 @@
 # Lesson 29
+def consonantCount(word, vowel = False):
+    counter = 0 #counts consonant and increase each time you find one
+    for char in word:
+        char = char.lower()
+        if char.isalpha() and char not in {"a","e","i","o","u"}:
+            counter += 1
+
+    #Extension, above is enough to just find consonants
+    if not vowel:
+        return counter
+    else:
+        counter = 0
+        for char in word:
+            char = char.lower()
+            if char.isalpha() and char in {"a","e","i","o","u"}:
+                counter += 1
+        return counter
+
+print(f"The count is {consonantCount("hello, world!", True)}")
