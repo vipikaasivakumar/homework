a472272acc09bcc59a93bd70822ac61f23903c21
vipikaasivakumar <vipikaasivakumar@gmail.com>
2026-03-22 15:18:17 -0400

    lesson 27 hw

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,11 @@
 # Lesson 27
+def stringClean (word):
+    result  = ""
+    for char in word:
+        #since strings are IMMutable can't mutate characters you don't want that's why we use an empty string and add
+        if char.isalpha():
+            result += char.lower()
+    return result
+
+value = stringClean(input())
+print(f"Value is {value}")
