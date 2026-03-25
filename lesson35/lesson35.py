56e67dd7c947f24edcfe0f9306117cc047c93612
vipikaasivakumar <vipikaasivakumar@gmail.com>
2026-03-24 23:33:38 -0400

    lesson 35 hw

--- lesson35/lesson35.py
+++ lesson35/lesson35.py
@@ -1,1 +1,12 @@
 # Lesson 35
+test = ["a", "b", "c", "c", "b", "c","a","a", "d"]
+print(f"Test list: {test}")
+
+def removeDuplicates(aList):
+    newList = []
+    for i in aList:
+        if i not in newList:
+            newList.append(i)
+    return newList
+    
+print(f"Duplicates removed: {removeDuplicates(test)}")
